77913276b7242447ce538feaaf9efee65f35cc23
Machine Learning Maverick <github@machinelearningmaverick.com>
2026-04-16 15:25:43 +0100

    feat(entities): add entities load for SQLAlchemy; introduce [project.script] for future use; along with proper logging

--- src/stellar_harvest_ie_store/db.py
+++ src/stellar_harvest_ie_store/db.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 from stellar_harvest_ie_store.settings import settings
 from stellar_harvest_ie_models.stellar.swpc.db import Base
+import stellar_harvest_ie_models.stellar.swpc.entities  # noqa: F401 - registers models with Base
 from stellar_harvest_ie_config.utils.log_decorators import log_io
 
 engine = create_async_engine(settings.database_url, echo=True)
--- src/stellar_harvest_ie_store/init_db.py
+++ src/stellar_harvest_ie_store/init_db.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 
 from stellar_harvest_ie_config.logging_config import setup_logging
@@ -6,10 +7,12 @@ setup_logging()
 
 from stellar_harvest_ie_store.db import init_db
 
+logger = logging.getLogger("stellar_harvest_ie_store.init_db")
 
 async def main():
     await init_db()
 
 
 if __name__ == "__main__":
+    logger.info("STORE module __main__")
     asyncio.run(main())
